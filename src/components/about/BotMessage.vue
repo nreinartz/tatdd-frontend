e59520264b62f0a6f15d5407152ab641f0497a86
Nico Reinartz <nico.reinartz@rwth-aachen.de>
2023-11-22 02:07:10 +0100

    fix: favicon

--- src/components/about/BotMessage.vue
+++ src/components/about/BotMessage.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="hstack gap-2 align-items-center">
-        <img src="favicon.ico" class="rounded d-block" style="max-height: 30px;">
+        <img src="/favicon.ico" class="rounded d-block" style="max-height: 30px;">
         <div class="card">
             <div class="card-body px-2 py-2" v-html="message">
             </div>
